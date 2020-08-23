3dbc219efc19746ac5b6a97dc30996ad862fdc2a
Denis Davydov <dendavydoff@gmail.com>
2020-08-23 10:05:50 +0300

    2308

--- sudoku.hs
+++ sudoku.hs
@@ -4,6 +4,17 @@ n = 9
 nm = 3
 nk = 81
 
+type Mat2d a = [[a]]
+type MatInt2d = Mat2d Int
+type Mat3d a = [[[a]]]
+type MatInt3d = Mat3d Int
+type ListM a = [a]
+type ListMInt =ListM Int
+type Pair a = (a,a)
+type PairInt = Pair Int
+type ListP a = [(a,a)]
+type ListPInt = ListP Int
+
 
 -------------------------------------------
 -- IO function
@@ -16,30 +27,54 @@ intArray x = do
     let int = map read (words str)::[Int]
     return (int:nextInt)
     
+print_mat2d :: MatInt2d -> String
+print_mat2d [] = ""
+print_mat2d x = (show (take 1 x)) ++ "\n" ++ (print_mat2d (drop 1 x))
+
+addSpaces :: Int -> String
+addSpaces 0 = ""
+addSpaces n = " " ++ addSpaces (n-1) 
+  
+show_d :: ListMInt -> String
+show_d [] = ""
+show_d (x:xs) = show x ++ show_d(xs)
+
+show_2d :: MatInt2d -> String
+show_2d [] = ""
+show_2d (x:xs) = addSpaces (n-length(dds)) ++ dds ++ " " ++ show_2d (xs)
+              where 
+              dds = show_d (x)
+
+print_mat3d :: MatInt2d -> String
+print_mat3d [] = ""
+print_mat3d x = (show_2d  (take n x)) ++ "\n" ++ (print_mat3d (drop n x))   
+    
 -------------------------------------------
 -- Conversion function
-convert_matrix_to_pair_list :: [[Int]] -> [(Int,Int,Int)]
+convert_matrix_to_pair_list :: MatInt2d -> [(Int,Int,Int)]
 convert_matrix_to_pair_list x 
         = map (\(i,j)->(i,j,(x !! i) !! j)) sort
          where indx  = [(i,j) | i<-[0..n-1],j<-[0..n-1]]
                sort     = filter (\ (ii,jj) ->  0 /= (x !! ii) !! jj) indx
 
-convert_matrix_to_stroke_list :: [[Int]] -> [(Int,Int)]
+convert_matrix_to_stroke_list :: MatInt2d -> [(Int,Int)]
 convert_matrix_to_stroke_list x 
         = map (\(i,j)->(i*n+j,(x !! i) !! j)) sort
          where indx  = [(i,j) | i<-[0..n-1],j<-[0..n-1]]
                sort  = filter (\ (ii,jj) ->  0 /= (x !! ii) !! jj) indx
 
-convert_matrix3d_to_stroke_list :: [[Int]] -> [(Int,Int)]
+convert_matrix3d_to_stroke_list :: MatInt2d -> [(Int,Int)]
 convert_matrix3d_to_stroke_list x 
         = map (\i->(i,(x !! i) !! 0)) sort
          where indx  = [i | i<-[0..nk-1]]
                sort  = filter (\ ii ->  length(x !! ii)==1) indx
 
-convert_listn_pair_to_listn = \x -> \y -> x*n+y
-convert_listn_to_pair = \x -> (x `div` n,x `mod` n)
+conv_p2l = \x -> \y -> x*n+y
+conv_l2p = \x -> (x `div` n,x `mod` n)
 fx = \x -> \p -> x+3*((get_quad_l p) `div` nm)
 fy = \x -> \p -> x+3*((get_quad_l p) `mod` nm)
+get_quad = \(x,y) -> (x `div` nm)*nm + y `div` nm
+get_quad_l = \p -> (\(x,y) -> (x `div` nm)*nm + y `div` nm )(conv_l2p p)
 
 ---------------------------------------------------
 -- Main function
@@ -50,7 +85,7 @@ gen_stroke_list 1 p =  [ i*n+p| i<-[0..(n-1)] ]
 -- strokes
 gen_stroke_list 2 p =  [ p*n+i| i<-[0..(n-1)] ]
 -- quadrant
-gen_stroke_list 3 p =  [ convert_listn_pair_to_listn (fx i p) (fy j p) | i<-[0..(nm-1)],j<-[0..(nm-1)] ]
+gen_stroke_list 3 p =  [ conv_p2l (fx i p) (fy j p) | i<-[0..(nm-1)],j<-[0..(nm-1)] ]
 
 gen_stroke_list_w:: Int -> Int -> (Int,Int)-> [Int]
 -- column
@@ -58,7 +93,7 @@ gen_stroke_list_w 1 p (x,y) =  [ i*n+p| i<-[0..(n-1)],i/=x ]
 -- strokes
 gen_stroke_list_w 2 p (x,y) =  [ p*n+i| i<-[0..(n-1)],i/=y ]
 -- quadrant
-gen_stroke_list_w 3 p (x,y) =  [ convert_listn_pair_to_listn (fx i p) (fy j p) | i<-[0..(nm-1)],j<-[0..(nm-1)],((fx i p)/=x)||((fy j p)/=y) ]
+gen_stroke_list_w 3 p (x,y) =  [ conv_p2l (fx i p) (fy j p) | i<-[0..(nm-1)],j<-[0..(nm-1)],((fx i p)/=x)||((fy j p)/=y) ]
 
 gen_stroke_list_wl:: Int -> Int -> [Int]
 -- column
@@ -66,9 +101,14 @@ gen_stroke_list_wl 1 p =  [ i*n+(p `mod` n)| i<-[0..(n-1)],i/=(p `div` n) ]
 -- strokes
 gen_stroke_list_wl 2 p =  [ (p `div` n)*n+i| i<-[0..(n-1)],i/=(p `mod` n) ]
 -- quadrant
-gen_stroke_list_wl 3 p =  [ convert_listn_pair_to_listn (fx i p) (fy j p) | i<-[0..(nm-1)],j<-[0..(nm-1)],((fx i p)/=(p `div` n))||((fy j p)/=(p `mod` n)) ]
-                            
+gen_stroke_list_wl 3 p =  [ conv_p2l (fx i p) (fy j p) | i<-[0..(nm-1)],j<-[0..(nm-1)],((fx i p)/=(p `div` n))||((fy j p)/=(p `mod` n)) ]
 
+union_2cell::[[Int]]->Int->Int->[Int]
+union_2cell t3d x1 x2 = []
+
+union_cell::[[Int]]->[Int]->[Int]
+union_cell t3d (x:xs) = []
+                            
 delete_from_list::[Int]->Int->[Int]
 delete_from_list t2d zz = filter (\x -> x/=zz) t2d
 
@@ -82,14 +122,6 @@ delete_from_cell_ t3d (x:xs) = delete_from_cell_ (delete_from_cell__ t3d x) xs
 set_to_cell__::[[Int]]->(Int,Int)->[[Int]]
 set_to_cell__ t3d (zz,zp) = [if i==zp then [zz] else (t3d!!i)|i<-[0..(nk-1)] ]
 
-get_quad :: (Int,Int) -> Int
-get_quad (x,y) = (x `div` nm)*nm + y `div` nm
-
-get_quad_l :: Int -> Int
-get_quad_l p = (x `div` nm)*nm + y `div` nm
-                where 
-                    (x,y)=convert_listn_to_pair p
-
 add_cell::[[Int]]->(Int,Int)->[[Int]]
 add_cell t3d (zp,zz) = delete_from_cell_ t3d1 (map   (\x -> (zz,x)) listxyz)
                     where t3d1=set_to_cell__ t3d (zz,zp) 
@@ -99,54 +131,31 @@ add_cell_list::[[Int]]->[(Int,Int)]->[[Int]]
 add_cell_list t3d [] = t3d
 add_cell_list t3d (x:xs) = add_cell_list (add_cell t3d x) xs
 
+is_correct::[[Int]]->Bool
+is_correct []=True
+is_correct (x:xs)=(length(x)/=0)&&is_correct(xs)
+
 init_table ::  [[Int]]
 init_table = replicate nk [1,2..9]
-
 
 main :: IO ()
 main =  do
 --mat<- intArray (n)
 let mat = [[ 0, 0, 0, 0, 0, 0, 4, 0, 0], [ 3, 0, 6, 0, 0, 0, 0, 0, 0], [ 0, 0, 0, 1, 9, 6, 0, 3, 0], [ 0, 7, 0, 0, 0, 0, 0, 1, 0], [ 8, 0, 0, 2, 5, 0, 0, 9, 0], [ 0, 4, 0, 0, 0, 0, 8, 0, 0], [ 0, 6, 0, 4, 0, 9, 0, 0, 8], [ 0, 0, 5, 0, 0, 0, 0, 2, 0], [ 0, 0, 0, 5, 0, 0, 0, 0, 7]]
-print (take 1 mat)
-print (take 1 (drop 1 mat))
-print (take 1 (drop 2 mat))
-print (take 1 (drop 3 mat))
-print (take 1 (drop 4 mat))
+putStrLn (print_mat2d mat)
 let list_matrix = convert_matrix_to_pair_list mat
---print list_matrix
---let gen_list=gen_stroke_list 3 3
---print gen_list
 let list_matrix2 = convert_matrix_to_stroke_list mat
-print list_matrix2
---let gen_list2=gen_stroke_list_w 3 3 (3,2)
---print gen_list2
+--putStrLn ( print_mat2d list_matrix2 )
+-------------------------------------------------------------
 let table3d = init_table
---print table3d
---let table3d1 = add_cell table3d (1,2)
 let table3d2 = add_cell_list table3d list_matrix2
-print (take 9 table3d2)
-print (take 9 (drop 9 table3d2))
-print (take 9 (drop 18 table3d2))
-print (take 9 (drop 27 table3d2))
-print (take 9 (drop 36 table3d2))
-print (take 9 (drop 45 table3d2))
-print (take 9 (drop 54 table3d2))
-print (take 9 (drop 63 table3d2))
-print (take 9 (drop 72 table3d2))
---let lst=gen_stroke_list_wl 3 22
---let lst2=map   (\x -> (x,9)) lst
---print lst2
+putStrLn ( print_mat3d table3d2 )
 let f=convert_matrix3d_to_stroke_list table3d2
 print f
-let table3d3 = add_cell_list table3d2 (foldl (flip delete) f list_matrix2)
-print (take 9 table3d3)
-print (take 9 (drop 9 table3d3))
-print (take 9 (drop 18 table3d3))
-print (take 9 (drop 27 table3d3))
-print (take 9 (drop 36 table3d3))
-print (take 9 (drop 45 table3d3))
-print (take 9 (drop 54 table3d3))
-print (take 9 (drop 63 table3d3))
-print (take 9 (drop 72 table3d3))
+let e =foldl (flip delete) f list_matrix2
+print e
+let table3d3 = add_cell_list table3d2 e
+putStrLn ( print_mat3d table3d3 )
+-------------------------------------------------------------
 let g=convert_matrix3d_to_stroke_list table3d3
 print g
